b2fa6e68364ba3dd6b2ca8a998360482645d505d
thaovi98 <128464228+thaovi98@users.noreply.github.com>
2023-04-04 21:42:39 +0700

    Add files via upload

---
+++ LAB5.sql
@@ -0,0 +1,119 @@
+﻿--1. Hãy xây dựng hàm đưa ra tên hangsx khi nhập vào masp từ bàn phím
+CREATE FUNCTION dbo.showSan_pham()
+RETURNS TABLE
+AS
+RETURN
+SELECT *
+FROM Sanpham;
+--2. Hãy xây dựng hàm đưa ra tổng giá trị nhập từ năm nhập x đến năm nhập y, với x,y được nhập vào từ bàn phím.
+CREATE FUNCTION TinhTongGiaTriNhap(@namx INT, @namy INT)
+RETURNS MONEY
+AS
+BEGIN
+    DECLARE @tongGiaTriNhap MONEY
+    SELECT @tongGiaTriNhap = SUM(dongiaN * soluongN)
+    FROM Nhap
+    WHERE YEAR(ngaynhap) >= @namx AND YEAR(ngaynhap) <= @namy
+    RETURN @tongGiaTriNhap
+END
+--3. Hãy viết hàm thống kê tổng số lượng thay đổi nhập xuất của tên sản phẩm x trong năm y, với x,y nhập từ bàn phím.
+CREATE FUNCTION ThongKeSoLuongNhapXuat(@tenSP NVARCHAR(50), @nam INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @soLuongNhapXuat INT
+
+    SELECT @soLuongNhapXuat = SUM(COALESCE(n.SoluongN, 0) - COALESCE(x.SoluongX, 0))
+    FROM SanPham sp
+    LEFT JOIN Nhap n ON sp.MaSP = n.MaSP
+    LEFT JOIN Xuat x ON sp.MaSP = x.MaSP AND YEAR(x.NgayXuat) = @nam
+    WHERE sp.TenSP = @tenSP AND YEAR(n.NgayNhap) = @nam
+
+    RETURN @soLuongNhapXuat
+END
+--4. Hãy xây dựng hàm đưa ra tổng giá trị nhập từ ngày nhập x đến ngày nhập y, với x, y được nhập vào từ bàn phím.
+CREATE FUNCTION TinhTongGiaTriNhapNgay(@ngayX DATE, @ngayY DATE)
+RETURNS MONEY
+AS
+BEGIN
+    DECLARE @tongGiaTriNhap MONEY
+
+    SELECT @tongGiaTriNhap = SUM(dongiaN * soluongN)
+    FROM Nhap
+    WHERE ngaynhap >= @ngayX AND ngaynhap <= @ngayY
+
+    RETURN @tongGiaTriNhap
+END
+--5. Hãy xây dựng hàm đưa ra tổng giá trị xuất của hãng tên hãng là A, trong năm tài khóa x, với A ,x được nhập từ bàn phím.
+CREATE FUNCTION fn_TongGiaTriXuat(@tenHang NVARCHAR(20), @nam INT)
+RETURNS MONEY
+AS
+BEGIN
+  DECLARE @tongGiaTriXuat MONEY;
+  SELECT @tongGiaTriXuat = SUM(S.giaban * X.soluongX)
+  FROM Xuat X
+  JOIN Sanpham S ON X.masp = S.masp
+  JOIN Hangsx H ON S.mahangsx = H.mahangsx
+  WHERE H.tenhang = @tenHang AND YEAR(X.ngayxuat) = @nam;
+  RETURN @tongGiaTriXuat;
+END;
+--6. Hãy xây dựng hàm thống kê số lượng nhân viên mỗi phòng với tên phòng nhập từ bàn phím.
+CREATE FUNCTION fn_ThongKeNhanVienTheoPhong (@tenPhong NVARCHAR(30))
+RETURNS TABLE
+AS
+RETURN
+    SELECT phong, COUNT(manv) AS soLuongNhanVien
+    FROM Nhanvien
+    WHERE phong = @tenPhong
+    GROUP BY phong;
+--7. Hãy viết hàm thống kê xem tên sản phẩm x đã xuất được bao nhiêu sản phẩm trong ngày y, với x,y nhập từ bản phím.
+CREATE FUNCTION sp_xuat_trong_ngay(@ten_sp NVARCHAR(20), @ngay_xuat DATE)
+RETURNS INT
+AS
+BEGIN
+  DECLARE @so_luong_xuat INT
+  SELECT @so_luong_xuat = SUM(soluongX)
+  FROM Xuat x JOIN Sanpham sp ON x.masp = sp.masp
+  WHERE sp.tensp = @ten_sp AND x.ngayxuat = @ngay_xuat
+  RETURN @so_luong_xuat
+END
+--8. Hãy viết hàm trả về số diện thoại của nhân viên đã xuất số hóa đơn x, với x nhập từ bàn phím.
+CREATE FUNCTION SoDienThoaiNV (@InvoiceNumber NCHAR(10))
+RETURNS NVARCHAR(20)
+AS
+BEGIN
+  DECLARE @EmployeePhone NVARCHAR(20)
+  SELECT @EmployeePhone = Nhanvien.sodt
+  FROM Nhanvien
+  INNER JOIN Xuat ON Nhanvien.manv = Xuat.manv
+  WHERE Xuat.sohdx = @InvoiceNumber
+  RETURN @EmployeePhone
+END
+--9. Hãy viết hàm thống kê tổng số lượng thay đổi nhập xuất của tên sản phẩm x trong năm y, với x,y nhập từ bàn phím.
+CREATE FUNCTION ThongKeSoLuongThayDoi(@tenSP NVARCHAR(20), @nam INT)
+RETURNS INT
+AS
+BEGIN
+  DECLARE @tongNhapXuat INT;
+  SET @tongNhapXuat = (
+SELECT COALESCE(SUM(nhap.soluongN), 0) + COALESCE(SUM(xuat.soluongX), 0) AS tongSoLuong
+    FROM Sanpham sp
+    LEFT JOIN Nhap nhap ON sp.masp = nhap.masp
+    LEFT JOIN Xuat xuat ON sp.masp = xuat.masp
+    WHERE sp.tensp = @tenSP AND YEAR(nhap.ngaynhap) = @nam AND YEAR(xuat.ngayxuat) = @nam
+  );
+  RETURN @tongNhapXuat;
+END;
+--10. Hãy viết hàm thống kê tổng số lượng sản phẩm của hãng x, với tên hãng nhập từ bàn phím.
+CREATE FUNCTION ThongkeSoluongSanphamHangsx(@tenhang NVARCHAR(20))
+RETURNS INT
+AS
+BEGIN
+    DECLARE @soluong INT;
+
+    SELECT @soluong = SUM(soluong)
+    FROM Sanpham sp JOIN Hangsx hs ON sp.mahangsx = hs.mahangsx
+    WHERE hs.tenhang = @tenhang;
+
+    RETURN @soluong;
+END;
